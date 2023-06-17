95dd17b49bcd6068905cec28e948fd47f37e2851
Sabirov Amir <superman.as13@gmail.com>
2023-06-17 08:42:45 +0300

    add smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+// Add spoiler on JS
 const acordion = document.getElementsByClassName('contentBox');
 
 for (i = 0; i<acordion.length; i++){
@@ -6,3 +7,18 @@ for (i = 0; i<acordion.length; i++){
 		this.classList.toggle('active')
 	})
 }
+
+//------------------------------------------------------------------------
+
+// Add smooth scroll on jQuery 
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top -10
+    }, 500);
+    return false;
+});
+// .top -10 - регулирует отступ сверху в пикселях
+// 500 - скорость скролла
+
+//------------------------------------------------------------------------
